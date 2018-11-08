3fbd59c1b2a39a5c5963a0e1f37b42f225618f96
Taiiayo <taiiakinyo@gmail.com>
2018-11-08 13:25:49 +0300

    class and two methods

---
+++ src/SlowPochta.Api/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlowPochta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        [Authorize]
+        [Route("getdeliveredmessages")]
+        public IActionResult GetDeliveredMessages()
+        {
+            return Ok();
+        }
+
+        [Authorize(Roles = "test")]
+        [Route("createmessage")]
+        public IActionResult CreateMessage()
+        {
+            return Ok();
+        }
+    }
+}
