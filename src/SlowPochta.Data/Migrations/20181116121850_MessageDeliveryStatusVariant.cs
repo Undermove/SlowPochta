334c74df64b6ac3148183ce96d2bcecb546cb8bb
Taiiayo <taiiakinyo@gmail.com>
2018-11-16 15:22:18 +0300

    28-SP/ added new model and migration for it

---
+++ src/SlowPochta.Data/Migrations/20181116121850_MessageDeliveryStatusVariant.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SlowPochta.Data.Migrations
+{
+    public partial class MessageDeliveryStatusVariant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageDeliveryStatusVariants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DeliveryStatusDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageDeliveryStatusVariants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageDeliveryStatusVariants");
+        }
+    }
+}
