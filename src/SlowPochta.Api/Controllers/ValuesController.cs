155e6b0b9fc0fba0765cc7ca44fd5201141f03f6
Undermove <undermove@yandex.ru>
2018-12-05 18:34:00 +0300

    37-SP. Add Swagger

--- src/SlowPochta.Api/Controllers/ValuesController.cs
+++ src/SlowPochta.Api/Controllers/ValuesController.cs
@@ -3,21 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SlowPochta.Api.Controllers
 {
-	[Route("api/[controller]")]
-	public class ValuesController : Controller
-	{
-		[Authorize]
-		[Route("getlogin")]
-		public IActionResult GetLogin()
-		{
-			return Ok($"Ваш логин: {User.Identity.Name}");
-		}
+	//[Route("api/[controller]")]
+	//public class ValuesController : Controller
+	//{
+	//	[Authorize]
+	//	[Route("getlogin")]
+	//	public IActionResult GetLogin()
+	//	{
+	//		return Ok($"Ваш логин: {User.Identity.Name}");
+	//	}
 
-		[Authorize(Roles = "test")]
-		[Route("getrole")]
-		public IActionResult GetRole()
-		{
-			return Ok("Ваша роль: администратор");
-		}
-	}
+	//	[Authorize(Roles = "test")]
+	//	[Route("getrole")]
+	//	public IActionResult GetRole()
+	//	{
+	//		return Ok("Ваша роль: администратор");
+	//	}
+	//}
 }
