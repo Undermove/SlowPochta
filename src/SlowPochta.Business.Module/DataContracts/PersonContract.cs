c7a5b98a54bacaa70b8950f3c078f4fc71452eb1
Dmitry <undermove@yandex.ru>
2019-03-26 15:18:25 +0300

    Add password min length and min login min length

--- src/SlowPochta.Business.Module/DataContracts/PersonContract.cs
+++ src/SlowPochta.Business.Module/DataContracts/PersonContract.cs
@@ -5,9 +5,11 @@ namespace SlowPochta.Business.Module.DataContracts
 	public class PersonContract
 	{
 		[Required]
+		[MinLength(2)]
 		public string Login { get; set; }
 
 		[Required]
+		[MinLength(6)]		
 		public string Password { get; set; }
 	}
 }
